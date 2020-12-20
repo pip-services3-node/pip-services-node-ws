e363b24ca061272a4dcdbbdb096b760e1f0c6108
Sergey Seroukhov <seroukhov@gmail.com>
2020-12-19 20:22:06 -0700

    Update and rename workspace.conf.ps1 to checkout.ps1

---
+++ checkout.ps1
@@ -0,0 +1,31 @@
+#!/usr/bin/env pwsh
+
+git clone https://github.com/pip-services/pip-services-devenv.git
+
+git clone https://github.com/pip-services3-node/pip-services3-commons-node.git
+git clone https://github.com/pip-services3-node/pip-services3-expressions-node.git
+git clone https://github.com/pip-services3-node/pip-services3-components-node.git
+git clone https://github.com/pip-services3-node/pip-services3-container-node.git
+git clone https://github.com/pip-services3-node/pip-services3-data-node.git
+git clone https://github.com/pip-services3-node/pip-services3-rpc-node.git
+git clone https://github.com/pip-services3-node/pip-services3-messaging-node.git
+git clone https://github.com/pip-services3-node/pip-services3-swagger-node.git
+git clone https://github.com/pip-services3-node/pip-services3-grpc-node.git
+git clone https://github.com/pip-services3-node/pip-services3-facade-node.git
+git clone https://github.com/pip-services3-node/pip-services3-elasticsearch-node.git
+git clone https://github.com/pip-services3-node/pip-services3-fluentd-node.git
+git clone https://github.com/pip-services3-node/pip-services3-memcached-node.git
+git clone https://github.com/pip-services3-node/pip-services3-redis-node.git
+git clone https://github.com/pip-services3-node/pip-services3-mongodb-node.git
+git clone https://github.com/pip-services3-node/pip-services3-mongoose-node.git
+git clone https://github.com/pip-services3-node/pip-services3-couchbase-node.git
+git clone https://github.com/pip-services3-node/pip-services3-sqlserver-node.git
+git clone https://github.com/pip-services3-node/pip-services3-sqlite-node.git
+git clone https://github.com/pip-services3-node/pip-services3-mysql-node.git
+git clone https://github.com/pip-services3-node/pip-services3-postgres-node.git
+git clone https://github.com/pip-services3-node/pip-services3-datadog-node.git
+git clone https://github.com/pip-services3-node/pip-services3-prometheus-node.git
+git clone https://github.com/pip-services3-node/pip-services3-seneca-node.git
+git clone https://github.com/pip-services3-node/pip-services3-mqtt-node.git
+git clone https://github.com/pip-services3-node/pip-services3-kafka-node.git
+git clone https://github.com/pip-services3-node/pip-services3-aws-node.git
